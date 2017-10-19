1aaae4d3e2078a13e2ef092a2c2fb61053f49902
M511-16\admin <s3A432058@student.nuct.edu.tw>
2017-10-19 16:06:14 +0800

    陳欣妤-從 HelloController 將參數傳遞至 view

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index($name='Everybody')
+    {
+        $data=['name'=>$name];
+        return view('hello.index',$data);
+    }
+
 }
