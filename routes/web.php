6cbe8f107db027147615a76280db2f319d8eb2a5
M511-16\admin <s3A432058@student.nuct.edu.tw>
2017-10-19 16:01:21 +0800

    陳欣妤-設定 Route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('home',function(){
 Route::get('home',function(){
     return view('home.index');
 });
+
+Route::get('hello/{name?}',[
+    'as'=>'hello.index',
+    'uses'=> 'HelloController@index'
+]);
